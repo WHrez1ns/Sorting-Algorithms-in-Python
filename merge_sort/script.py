4ada968182955c825c7eb6c3a8adcad58efa4c40
Renan Dias <112900871+WHrez1ns@users.noreply.github.com>
2023-05-05 08:49:46 -0300

    Adicionando mais features
    - Biblioteca Matplotlib para transformar dados em gráficos
    - Adicionando mais de um teste
    - Adicionando o cálculo de média

--- merge_sort/script.py
+++ merge_sort/script.py
@@ -1,18 +1,37 @@
 from functions import merge_sort
 import time
-
+import matplotlib.pyplot
 
 with open("wordlists/probable-v2-top12000.txt", "r") as arquivo:
     lista = arquivo.read().splitlines()
-
-
-start_time = time.time()
-ordenada = merge_sort(lista)
-end_time = time.time()
-total_time = end_time - start_time
+testes = ['1', '2', '3', '4', '5', '6', '7',
+          '8', '9', '10', '11', '12', '13', '14', '15']
+result = []
+cont = 0
+total = 0
+while cont < 15:
+    start_time = time.time()
+    ordenada = merge_sort(lista)
+    end_time = time.time()
+    total_time = end_time - start_time
+    result.append(float(total_time))
+    print("\033[30m--------------------------------------------------------")
+    print("\033[34m", ordenada)
+    print("\033[30m--------------------------------------------------------")
+    print("\033[32mTempo de execução: {:.5f} segundos".format(total_time))
+    print("\033[30m--------------------------------------------------------")
+    print("\033[36mTamanho da wordlist: {} palavras".format(len(lista)))
+    cont += 1
+media = sum(result) / len(result)
 print("\033[30m--------------------------------------------------------")
-print("\033[34m", ordenada)
+print("\033[33mTestes realizados:", result)
 print("\033[30m--------------------------------------------------------")
-print("\033[32mTempo de execução: {:.5f} segundos".format(total_time))
+print("\033[35mMédia de tempo: {}".format(media))
 print("\033[30m--------------------------------------------------------")
-print("\033[36mTamanho da wordlist: {} palavras".format(len(lista)))
+matplotlib.pyplot.title(
+    'Média do tempo de execução do Algoritmo de Merge Sort')
+matplotlib.pyplot.xlabel('Teste')
+matplotlib.pyplot.ylabel('Tempo (s)')
+matplotlib.pyplot.plot(testes, result)
+matplotlib.pyplot.ylim(0, 10)
+matplotlib.pyplot.show()
